aa7853793fe1eb8e33fbc639dc2cc1411f0253cc
Thomas McMahon <tmcmahon@nerdery.com>
2020-07-06 16:08:21 -0500

    Updating version number

--- daily-co.php
+++ daily-co.php
@@ -7,7 +7,7 @@
  * Author URI:      https://www.twistermc.com/
  * Text Domain:     daily_co
  * Domain Path:     /languages
- * Version:         0.3.0
+ * Version:         0.3.1
  *
  * @package         Daily_Co
  */
